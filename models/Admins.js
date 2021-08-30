ec32adea384e00ec5d90b5dae224c8514ae7ee46
Chibykes <62060851+Chibykes@users.noreply.github.com>
2021-08-30 13:40:58 +0100

    Update Admins.js

--- models/Admins.js
+++ models/Admins.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const AdminSchema = new Schema({
     adminID: {type: String, lowercase: true},
     fullname: {type: String, lowercase: true},
-    password: {type: String, lowercase: true}
+    password: {type: String}
 });
 
 module.exports = mongoose.model('admins', AdminSchema);
